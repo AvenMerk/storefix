b1930a5708d0d9448890f7c94acb127bf358c680
avenmerk <=>
2019-02-13 15:11:25 +0300

    add field to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Field from './components/field.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-
+            <Field />
+            <Field />
+            <Field />
         </header>
       </div>
     );
