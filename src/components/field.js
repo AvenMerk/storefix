112fabea503112ada4c71d00e10d67499610d1b1
avenmerk <=>
2019-02-13 15:20:05 +0300

    edit field structure

--- src/components/field.js
+++ src/components/field.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Field extends Component {
-    render() {
-        return (
-            <div>
-                <form>
-                    <select name="options">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                    </select>
-                </form>
-            </div>
-        );
-    }
-}
+const Field = () => (
+    <div>
+        <form>
+            <select name="options">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+            </select>
+        </form>
+    </div>
+);
 
 export default Field;
