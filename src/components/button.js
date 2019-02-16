f81370a179be8d5197f8252081d14e21266e9313
AvenMerk <kremnevaaj@gmail.com>
2019-02-16 17:33:56 +0300

    add arrow icon to button

--- src/components/button.js
+++ src/components/button.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Button = ({buttonName, onClickFunction}) => (
     <div>
-        <button onClick={onClickFunction} className="button">{buttonName}</button>
+        <button onClick={onClickFunction} className="button-container">
+
+            <span>{buttonName}</span>
+            <span className="button-light-arrow-down" />
+        </button>
     </div>
 );
 
